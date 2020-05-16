f938d10cafcd5ee4f6d3df5791ec38bd7836457d
Maxime Robion <max.robion@me.com>
2020-05-16 11:09:44 +0200

    new Review model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+
+
+def index
+  # this is our list page for our reviews
+  @number  = rand(100)
+  @reviews = ["The Smile", "Baby Bo's", "Chipotle", "Albiom", "McDonalds"]
+end
+
+def new
+
+end
+
 end
