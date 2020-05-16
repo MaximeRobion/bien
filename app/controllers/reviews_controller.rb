3e17975467fffaf21b8af6480f8f0ba06bed22c6
Maxime Robion <max.robion@me.com>
2020-05-16 12:37:53 +0200

    week1 - scaffold reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,61 @@
 class ReviewsController < ApplicationController
 
 
-def index
-  # this is our list page for our reviews
-  @number  = rand(100)
-  @reviews = ["The Smile", "Baby Bo's", "Chipotle", "Albiom", "McDonalds"]
-end
+  def index
+    # this is our list page for our reviews
+    @reviews = Review.all
+  end
 
-def new
+  def new
+    @review = Review.new
+  end
 
-end
+
+  def create
+    #take the info from the for and add it to the database
+    @review = Review.new(form_params)
+
+    #save this to the database
+    @review.save
+
+    #redirect back to the homepage
+    redirect_to root_path
+  end
+
+  def show
+    #individual review page
+    @review = Review.find(params[:id])
+  end
+
+  def destroy
+    #find the individiual review
+    @review = Review.find(params[:id])
+
+    #destroy
+    @review.destroy
+
+    #redirect to the homepage
+    redirect_to root_path
+  end
+
+  def edit
+    #find the individual review (to edit)
+    @review = Review.find(params[:id])
+  end
+
+  def update
+        #find the individual review
+        @review = Review.find(params[:id])
+
+        #update with the new form info
+        @review.update(form_params)
+
+        #redirect somewhere new
+        redirect_to review_path(@review)
+  end
+
+  def form_params
+    params.require(:review).permit(:title, :body, :score)
+  end
 
 end
